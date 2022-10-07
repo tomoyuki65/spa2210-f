1d350e3bf6e46c68eb7acd7864bd4f63e7d69ea7
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2022-10-08 02:22:47 +0900

    add tailwindcss & jest

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+  return (
+    <h1 className="text-blue-700 underline decoration-gray-500">
+      Hello World!
+    </h1>
+  )
+}
+
+export default About
