8f17fea21d0d7f5b7ecdee16d893178df098e0b3
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2023-01-15 20:54:13 +0900

    add axios

--- components/likebutton.tsx
+++ components/likebutton.tsx
@@ -1,11 +1,13 @@
 /* 「いいねボタン」のコンポーネント */
 
 import { useState } from "react"
+import { toast } from "react-toastify";
 
 const LikeButton = () => {
   const [count, setCount] = useState(99);
   const handleClick = () => {
     setCount(count + 1);
+    toast.error("アラートのメッセージ");
   };
   return (
     <span className="bg-red-500 text-white p-[0.8rem] rounded-md cursor-pointer inline-block"
